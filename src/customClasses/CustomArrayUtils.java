9301b028193cece28cfbd7109df9a8840e9c80bf
Andrey <volgin_andrey27@mail.ru>
2024-07-09 09:38:55 +0500

    Update CustomArrayUtils.java

--- src/customClasses/CustomArrayUtils.java
+++ src/customClasses/CustomArrayUtils.java
@@ -8,8 +8,8 @@ import java.util.List;
 public class CustomArrayUtils {
     public static Object[] filter(Object[] array, Filter filter) {
         List<Object> result = new ArrayList<>();
-        for (Object o : array) {
-            result.add(filter.apply(o));
+        for (Object element : array) {
+            result.add(filter.apply(element));
         }
         return result.toArray(new Object[0]);
     }
